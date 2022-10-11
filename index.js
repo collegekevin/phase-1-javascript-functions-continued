2237e423aa93254ac421b131f0e6e9da8bb4e8ff
Kevin Price <collegekevin@gmail.com>
2022-10-10 17:07:55 -0700

    commit tree c5a9bf4d54830e2bd060cee5463271c2f10876e6

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+function saturdayFun (activity = "roller-skate"){
+    return "This Saturday, I want to " + activity + "!";
+}
+
+function mondayWork (toDo = "go to the office"){
+    return "This Monday, I will " + toDo + ".";
+}
+
+function wrapAdjective (flair='*'){
+    return function (word = "special"){
+        return `You are ${flair}${word}${flair}!`
+    }
+            
+    
+}
